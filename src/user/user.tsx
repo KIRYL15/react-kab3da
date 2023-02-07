64b7189bfdf76bea83a04c52ce6213a39c546fce
Kirill Pravdin <kirill-pravdin@mail.ru>
2023-02-07 21:31:32 +0300

    Commit test-5

--- src/user/user.tsx
+++ src/user/user.tsx
@@ -4,9 +4,8 @@ const User = () => {
     return (
         <div>
             <h1>post122</h1>
-            <h1>post122</h1>
-            <h1>6669</h1>
-            <h1>1265</h1>
+            <h1>POST11</h1>
+
         </div>
     );
 };
